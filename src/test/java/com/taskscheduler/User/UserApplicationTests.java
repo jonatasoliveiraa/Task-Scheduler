ec7ee8df6b4eb8d3a4a9fad7a7c64717d6962011
Jonatas Oliveira <jonatasoliveiramont@gmail.com>
2025-02-24 13:35:21 -0300

    Initial commit

---
+++ src/test/java/com/taskscheduler/User/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskscheduler.User;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
